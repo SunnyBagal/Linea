ec3e3923ca34a41a972d7a0d0ae876ba17c05770
Sinister <sunnybagal.1110@gmail.com>
2026-06-19 20:27:30 +0530

    Better type checking

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,16 +1,55 @@
-import { email, z } from 'zod';
+import { z } from 'zod';
 
 export const CreateUserSchema = z.object({
-  username: z.string(),
+  username: z.string().min(3).max(20),
   email: z.email(),
-  password: z.string(),
-})
+  password: z.string().min(8).max(100),
+});
 
 export const SigninSchema = z.object({
   email: z.email(),
-  password: z.string(),
-})
+  password: z.string().min(8).max(100),
+});
 
 export const CreateRoomSchema = z.object({
-  slug : z.string().min(3).max(20)
-})
+  slug: z.string().min(3).max(20),
+});
+
+export const ShapeSchema = z.object({
+  type: z.enum(['rect', 'ellipse', 'line', 'arrow', 'text', 'freedraw']),
+  x: z.number(),
+  y: z.number()
+});
+
+const JoinRoomSchema = z.object({
+  type: z.literal('join_room'),
+  roomId: z.number().int()
+});
+
+const LeaveRoomSchema = z.object({
+  type: z.literal('leave_room'),
+  roomId: z.number().int()
+});
+
+const ChatSchema = z.object({
+  type: z.literal('chat'),
+  roomId: z.number().int(),
+  message: z.string().min(1).max(2000)
+});
+
+const OpSchema = z.object({
+  type: z.literal('op'),
+  roomId: z.number().int(),
+  opType: z.enum(['CREATE','UPDATE','DELETE']),
+  payload: ShapeSchema.nullable(),
+});
+
+export const WsMessageSchema = z.discriminatedUnion('type', [
+  JoinRoomSchema,
+  LeaveRoomSchema,
+  ChatSchema,
+  OpSchema,
+]);
+
+export type WsMessage = z.infer<typeof WsMessageSchema>;
+export type OpMessage = z.infer<typeof OpSchema>;
