7362cb43c66d8486f60f618dede30fed1ecd5f70
Sinister <sunnybagal.1110@gmail.com>
2026-06-21 16:30:10 +0530

    basic canvas working

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,23 +1,52 @@
-import { useEffect, useRef } from "react"
+"use client";
 
-export default function Canvas () {
+import { useEffect, useRef } from "react";
 
-  const canvasRef = useRef();
+export default function Canvas() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-    if (canvasRef.current) {
-      
-    }
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    let clicked = false;
+    let startX = 0;
+    let startY = 0;
+
+    canvas.addEventListener("mousedown", (e) => {
+      clicked = true;
+      startX = (e.clientX)
+      startY = (e.clientY)
+    })
+
+     canvas.addEventListener("mouseup", (e) => {
+      clicked = false
+      console.log(e.clientX)
+      console.log(e.clientY)
+    })
+
+     canvas.addEventListener("mousemove", (e) => {
+       if (clicked){
+          const width = e.clientX - e.clientY
+          const height = e.clientY - e.clientX
+
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.strokeRect(startX,startY, width, height)
+       }
+    })
+
+    ctx.strokeRect(20, 20, 50, 100);
 
 
-  }, [canvasRef]);
+  }, []);
 
   return (
     <div>
-      <canvas className="w-125 h-125 bg-amber-50"></canvas>
+      <canvas ref={canvasRef} className="bg-amber-50 w-125 h-125" />
       Hi there
-      
     </div>
-  )
+  );
 }
